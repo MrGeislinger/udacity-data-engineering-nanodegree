bc794e1731cf39c18b6091e037e81e687bc986ee
MrGeislinger <GitHub@MrGeislinger.com>
2020-04-27 21:27:55 -0700

    feat: Process song data function

--- etl.py
+++ etl.py
@@ -23,22 +23,63 @@ def create_spark_session():
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = 
+    song_data = '{}/song_data'.format(input_data)
     
     # read song data file
-    df = 
+    df = spark.read.json(song_data)
 
     # extract columns to create songs table
-    songs_table = 
+    songs_table = df.select(
+                        'song_id',
+                        'title',
+                        'artist_id',
+                        'year',
+                        'duration'
+                    ).dropDuplicates()
+    songs_table.createOrReplaceTempView('songs')
     
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table.write.partitionBy(
+                        'year',
+                        'artist_id'
+                    ).parquet(
+                        os.path.join(
+                            output_data,
+                            'songs/songs.parquet'
+                        ),
+                        'overwrite'
+                    )
 
     # extract columns to create artists table
-    artists_table = 
+    artists_table = df.select(
+                        'artist_id',
+                        'artist_name',
+                        'artist_location',
+                        'artist_latitude',
+                        'artist_longitude'
+                    ).withColumnRenamed(
+                        'artist_name',
+                        'name'
+                    ).withColumnRenamed(
+                        'artist_location',
+                        'location'
+                    ).withColumnRenamed(
+                        'artist_latitude',
+                        'latitude'
+                    ).withColumnRenamed(
+                        'artist_longitude',
+                        'longitude'
+                    ).dropDuplicates()
+    artists_table.createOrReplaceTempView('artists') 
     
     # write artists table to parquet files
-    artists_table
+    artists_table.write.parquet(
+                            os.path.join(
+                                output_data,
+                                'artists/artists.parquet'
+                            ),
+                            'overwrite'
+                        )
 
 
 def process_log_data(spark, input_data, output_data):
@@ -83,7 +124,7 @@ def process_log_data(spark, input_data, output_data):
 
 def main():
     spark = create_spark_session()
-    input_data = "s3a://udacity-dend/"
+    input_data = "s3a://udacity-dend"
     output_data = ""
     
     process_song_data(spark, input_data, output_data)    
